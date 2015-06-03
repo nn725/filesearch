7a50f2b533bc4e7c501e7dc9704e29a1c2fcc254
Nathan <nouyang48@gmail.com>
2015-06-03 13:38:17 -0700

    final

--- filesearch.py
+++ filesearch.py
@@ -5,7 +5,7 @@ import requests
 
 import os
 
-
+API_KEY = os.environ.get('API_KEY')
 
 #configuration
 DATABASE = '/tmp/filesearch.db'
@@ -22,13 +22,13 @@ def init_db():
 			db.cursor().executescript(f.read())
 		db.commit()
 
-@app.cli.command('initdb')
-def initdb_command():
-	init_db()
-	print('Initialized the database')
+# @app.cli.command('initdb')
+# def initdb_command():
+# 	init_db()
+# 	print('Initialized the database')
 
 def get_db():
-	if not hasattr(g 'sqlite_db'):
+	if not hasattr(g, 'sqlite_db'):
 		g.sqlite_db = connect_db()
 	return g.sqlite_db
 
@@ -39,42 +39,37 @@ def close_db(error):
 
 @app.route('/')
 def index():
-	return render_template('index.html', appId=os.environ['APP_ID'], results=[])
+	return render_template('show_results.html', appId=os.environ['APP_ID'])
 
-@app.route('/search')
+@app.route('/search', methods=['POST'])
 def search():
 	return show_results(request.form['query'])
 
 #@app.route('/results/<query>')
 def show_results(query):
 	#retrieve account data from db
-	cur = g.db.execute('select service, key from accounts order by id desc')
+	db = get_db()
+	cur = db.execute('select service, key from accounts order by id desc')
 	accounts = [dict(service=row[0], key=row[1]) for row in cur.fetchall()]
-	#get list of dictionaries by searching
-	lst = []
-	for (account in accounts):
-		url = 'https://api.kloudless.com:443/v0/accounts/{0}/search/?q={1}'.format(account[key], query)
-		lst.append([account.service, requests.get(url).contents])
+
+	account_string = ','.join([str(a['key']) for a in accounts])
+
+	url = 'https://api.kloudless.com:443/v0/accounts/{0}/search/?q={1}'.format(account_string, query)
+
+	resp = requests.get(url, headers={"Authorization": "ApiKey %s" % API_KEY})
+	if resp.ok:
+		dic = resp.json()
+
 	#change lst to list of dictionary with service = service, and results = string of results
-	results = helper(lst)
+	results = [dict(name=a['name']) for a in dic['objects']]
 	return render_template('show_results.html', appId=os.environ['APP_ID'], results=results)
 
-def helper(lst):
-	d = []
-	for piece in lst:
-		s = ''
-		for o in piece[1]['objects']:
-			s = s + ', ' + o
-		d.append({'service' : piece[0], 'results' : s[2:]})
-	return d
-
-@app.route('/add/<service>/<int:key>', methods=['POST'])
-def add_account(service, key):
-	db = get_db()
-	db.execute('insert into accounts (service, key) values (?, ?)', [service, key])
-	g.db.commit()
-	flash("Account added")
-	return redirect(url_for('show_results'))
+@app.route('/add', methods=['POST'])
+def add_account():
+		db = get_db()
+		db.execute('insert into accounts (service, key) values (?, ?)', [request.form['service'], request.form['key']])
+		db.commit()
+		return render_template('show_results.html', appId=os.environ['APP_ID'])
 
 def main():
 	for k in ['API_KEY', 'APP_ID']:
@@ -82,6 +77,7 @@ def main():
 			print('error')
 			return
 
+	init_db()
 	app.run()
 
 if __name__ == '__main__':
